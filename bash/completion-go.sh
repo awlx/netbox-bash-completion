90d5ef7b230e93d6cdbc2e79ec0898f5eb2335d7
Annika Wickert <aw@awlnx.space>
2018-09-05 08:04:50 +0200

    Fixes for auto-completion and don't dump errors to auto-completion

--- bash/completion-go.sh
+++ bash/completion-go.sh
@@ -1,10 +1,11 @@
 #/usr/bin/env bash
 _netbox_completions()
 {
-ARGS=`$GOPATH/bin/netbox-autocompletion -netbox-api-token <netbox-api-token> -netbox-device ${COMP_WORDS[1]}`
-for i in $ARGS; do
-    COMPREPLY+=("$i")
-done;
+ARGS=`$GOPATH/bin/netbox-autocompletion -netbox-api-token <netbox-api-token> -netbox-device ${COMP_WORDS[1]} 2> /dev/null`
+
+if [ $? -eq 0 ]; then
+COMPREPLY=($(compgen -W "$ARGS" -- ${COMP_WORDS[COMP_CWORD]} ))
+fi
 }
 
 complete -F  _netbox_completions ssh
