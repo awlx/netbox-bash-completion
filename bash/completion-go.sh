57c256a944f2021cc23ed7db423043fa983e88b7
Annika Wickert <awlx@users.noreply.github.com>
2019-02-18 00:27:43 +0100

    Update completion-go.sh
    fix wrong path

--- bash/completion-go.sh
+++ bash/completion-go.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 _netbox_completions()
 {
-ARGS=`$GOPATH/bin/netbox-autocompletion -netbox-api-token <netbox-api-token> -netbox-device ${COMP_WORDS[1]} 2> /dev/null`
+ARGS=`$GOPATH/bin/netbox-bash-completion -netbox <netbox_url> -netbox-api-token <token> -netbox-device ${COMP_WORDS[1]} -tld "" 2> /dev/null`
 
 if [ $? -eq 0 ]; then
 COMPREPLY=($(compgen -W "$ARGS" -- ${COMP_WORDS[COMP_CWORD]} ))
